b875f936100f1da54f058b2919ab13c824c494a5
galaxyuliana <yulianashin613@gmail.com>
2022-09-22 21:53:03 -0700

    Added redirecing and wildcard routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ const appRoutes: Routes = [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent },
   ] },
+  {path:'not-found', component: PageNotFoundComponent},
+  {path:'**', redirectTo: '/not-found'},
 ];
 
 @NgModule({
